ae21ea45ad85b8f391ba666149e9500dfdcf3b6a
Rayane93380 <133586286+Rayane93380@users.noreply.github.com>
2024-05-03 10:58:58 +0200

    Update SignUp.php

--- WelcomePage/SignUpPage/SignUp.php
+++ WelcomePage/SignUpPage/SignUp.php
@@ -1,20 +1,20 @@
 <?php
 
-function signUp($email, $password) {
+function signUp($pseudo, $password) {
     $file_users = 'users.txt';
 
-    // Vérifier si l'adresse email existe déjà dans le fichier des utilisateurs
+    // Vérifier si l'adresse pseudo existe déjà dans le fichier des utilisateurs
     $users = file($file_users, FILE_IGNORE_NEW_LINES);
     foreach ($users as $user) {
-        list($existing_email, $existing_password) = explode('|', $user);
-        if ($existing_email === $email) {
+        list($existing_pseudo, $existing_password) = explode('|', $user);
+        if ($existing_pseudo === $pseudo) {
             
-            return "Erreur : Cette adresse email est déjà utilisée.";
+            return "Erreur : Ce pseudo est déjà utilisée.";
         }
     }
 
-    // L'adresse email n'existe pas, ajouter le nouvel utilisateur
-    $new_user = "$email|$password" . PHP_EOL;
+    // L'adresse pseudo n'existe pas, ajouter le nouvel utilisateur
+    $new_user = "$pseudo|$password" . PHP_EOL;
     if (file_put_contents($file_users, $new_user, FILE_APPEND | LOCK_EX) === false) {
         return "Erreur lors de l'inscription de l'utilisateur.";
     }
@@ -25,11 +25,11 @@ function signUp($email, $password) {
 // Vérifier si le formulaire a été soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupérer les valeurs saisies par l'utilisateur depuis le formulaire
-    $email = $_POST['Email'];
+    $pseudo = $_POST['pseudo'];
     $password = $_POST['Password'];
 
     // Appeler la fonction signUp avec les informations récupérées depuis le formulaire
-    $resultat = signUp($email, $password);
+    $resultat = signUp($pseudo, $password);
 
     // Afficher le résultat de l'inscription
     echo $resultat;
@@ -38,3 +38,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 ?>
+
