8903ea7a01e7652c6190f8503ba0525d31e8639d
Rayane93380 <133586286+Rayane93380@users.noreply.github.com>
2024-05-03 11:31:32 +0200

    Update Welcome.php

--- WelcomePage/Welcome.php
+++ WelcomePage/Welcome.php
@@ -22,8 +22,9 @@
             <form action="/ConnexionPage/Connexion.php" method="post" id="Connexion">
                 <div class="TitleForm"><p>CONNEXION</p></div>
                 <div class="ConnexionForm">
-                    <label for="Pseudo">PSEUDO</label>
-                    <input name="Pseudo" placeholder="Pseudo">
+                <label for="pseudo">PSEUDO</label>
+                <input name="pseudo" placeholder="Pseudo">
+
                     <label for="Password">MOT DE PASSE</label>
                     <input name="Password" placeholder="Mot de passe">
                     <div class="Option">
@@ -38,7 +39,7 @@
                 <div class="TitleForm"><p>S'INSCRIRE</p></div>
                 <div class="ConnexionForm">
                     <label for="pseudo">PSEUDO</label>
-                    <input name="Pseudo" placeholder="Pseudo">
+                    <input name="pseudo" placeholder="Pseudo">
                     <label for="Password">MOT DE PASSE</label>
                     <input name="Password" placeholder="Mot de passe">
                     <input type="submit" value="S'inscrire">
@@ -56,7 +57,9 @@
         </div>
         <div class="content-c2-l3">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dignissim dapibus orci quis ornare. Fusce neque elit, malesuada non nisl et, molestie pretium nunc. Donec nec imperdiet diam. Aenean ultrices dui elit, a imperdiet justo iaculis eu. Fusce laoreet congue enim, ut tincidunt massa scelerisque ac. Donec magna sem, rhoncus et felis id, consequat volutpat eros. Curabitur a tempor ligula. Integer malesuada, velit sit amet vehicula tempor, diam justo egestas eros, ac lacinia quam odio eu diam. Cras sodales enim felis, vitae gravida lacus porta eget. Etiam consectetur eget neque at auctor. </p>
-        </div>
+        </div>   </div>
+        <div class="content-c2-l3">
+     
         <div class="content-c1-l4">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dignissim dapibus orci quis ornare. Fusce neque elit, malesuada non nisl et, molestie pretium nunc. Donec nec imperdiet diam. Aenean ultrices dui elit, a imperdiet justo iaculis eu. Fusce laoreet congue enim, ut tincidunt massa scelerisque ac. Donec magna sem, rhoncus et felis id, consequat volutpat eros. Curabitur a tempor ligula. Integer malesuada, velit sit amet vehicula tempor, diam justo egestas eros, ac lacinia quam odio eu diam. Cras sodales enim felis, vitae gravida lacus porta eget. Etiam consectetur eget neque at auctor. </p>
         </div>
@@ -67,7 +70,9 @@
 
 
     <!--Footer-->
-    <div class="Footer">
+    <div class="   </div>
+        <div class="content-c2-l3">
+     Footer">
         <div classem
         <div class="col3-lg1">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dignissim dapibus orci quis ornare. Fusce neque elit, malesuada non nisl et, molestie pretium nunc. Donec nec imperdiet diam. Aenean ultrices dui elit, a imperdiet justo iaculis eu. Fusce laoreet congue enim, ut tincidunt massa scelerisque ac. Donec magna sem, rhoncus et felis id, consequat volutpat eros. Curabitur a tempor ligula. Integer malesuada, velit sit amet vehicula tempor, diam justo egestas eros, ac lacinia quam odio eu diam. Cras sodales enim felis, vitae gravida lacus porta eget. Etiam consectetur eget neque at auctor. </p>
@@ -75,6 +80,8 @@
     </div>
     </body>
 </html>
+
+
 
 
 em
