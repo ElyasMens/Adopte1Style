344671179778f7d5f47fbe0301ef78eb53c45fef
ElyasMens <133586599+ElyasMens@users.noreply.github.com>
2024-05-03 23:51:14 +0200

    Update SignUp.php
    Modification des valeurs récupérées pour l'inscription

--- SignUpConnexionFolder/SignUpPage/SignUp.php
+++ SignUpConnexionFolder/SignUpPage/SignUp.php
@@ -1,18 +1,18 @@
 <?php
 function signUp($pseudo, $password) {
-    $file_users = 'users.txt';
+    $file_users = '../identifier.txt';
 
-    // Vérifier si le pseudo existe déjà dans le fichier des utilisateurs
+    // Vérifie si le pseudo existe déjà dans le fichier des utilisateurs
     $users = file($file_users, FILE_IGNORE_NEW_LINES);
     foreach ($users as $user) {
-        list($existing_pseudo, $existing_password, $role) = explode('|', $user);
+        list($existing_pseudo, $existing_password, $existing_category) = explode(';', $user);
         if ($existing_pseudo === $pseudo) {
             return "Erreur : Ce pseudo est déjà utilisé.";
         }
     }
 
-    // Le pseudo n'existe pas, ajouter le nouvel utilisateur
-    $new_user = "$pseudo|$password|user" . PHP_EOL;
+    // Le pseudo n'existe pas, ajoute le nouvel utilisateur 
+    $new_user = "$pseudo;$password;user" . PHP_EOL;
     if (file_put_contents($file_users, $new_user, FILE_APPEND | LOCK_EX) === false) {
         return "Erreur lors de l'inscription de l'utilisateur.";
     }
@@ -20,26 +20,18 @@ function signUp($pseudo, $password) {
     return "Inscription réussie !";
 }
 
-// Vérifier si le formulaire d'inscription a été soumis
+// Vérifie si le formulaire a été soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupérer les valeurs saisies par l'utilisateur depuis le formulaire
-    $pseudo = $_POST['pseudo'];
-    $password = $_POST['Password'];
+    $pseudo = $_POST['identifier'];
+    $password = $_POST['password'];
 
-    // Appeler la fonction signUp() avec les informations récupérées depuis le formulaire
+    // Appeler la fonction signUp avec les informations récupérées depuis le formulaire
     $resultat = signUp($pseudo, $password);
 
-    // Vérifier si l'inscription a été réussie
-    if ($resultat === "Inscription réussie !") {
-        // Rediriger l'utilisateur vers la page de création de profil
-        header("Location: Profile.php");
-        exit; // Assurez-vous de terminer le script après la redirection
-    } else {
-        // Afficher un message d'erreur si l'inscription a échoué
-        echo $resultat;
-    }
+    // Afficher le résultat de l'inscription
+    echo $resultat;
 } else {
-    // Afficher un message d'erreur si le formulaire n'a pas été soumis
     echo "Erreur : Le formulaire n'a pas été soumis.";
 }
 ?>
