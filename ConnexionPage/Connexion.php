410df83df35bb29a45272f1f135aacac526eb2f6
ElyasMens <133586599+ElyasMens@users.noreply.github.com>
2024-05-03 02:13:32 +0200

    Add files via upload

---
+++ ConnexionPage/Connexion.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Connexion</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="Connexion.css">
+    </head>
+    <body>
+        <form action="Session.php" method="post">
+            <div><label for="Pseudo">Pseudo</label><input name="Pseudo"></div>
+                <div><label for="Mdp">Mot de passe</label><input name="Mdp"></div>
+                    <input class="submit" type="submit">           
+        </form>
+    </body>
+</html>
---
+++ HomePage/Home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Adopte1Style</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="Home.css">
+        <script src="Home.js"></script>
+    </head>
+    <body>
+        
+    <div class="Header"><a href="../ConnexionPage/Connexion.php">Se connecter </a><span>|</span><a href="../RegisterPage/Register.php"> S'inscrire</a></div>
+    <div ><img src="anImage.png" alt="image" id="first" width="100px" height="100px"></div>
+    </body>
+</html>
