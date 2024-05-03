06baa9d19cc98905ebb805cf26d492b76f903554
Rayane93380 <133586286+Rayane93380@users.noreply.github.com>
2024-05-03 11:32:04 +0200

    Update SignUp.php

--- WelcomePage/SignUpPage/SignUp.php
+++ WelcomePage/SignUpPage/SignUp.php
@@ -1,19 +1,17 @@
 <?php
-
 function signUp($pseudo, $password) {
     $file_users = 'users.txt';
 
-    // Vérifier si l'adresse pseudo existe déjà dans le fichier des utilisateurs
+    // Vérifier si le pseudo existe déjà dans le fichier des utilisateurs
     $users = file($file_users, FILE_IGNORE_NEW_LINES);
     foreach ($users as $user) {
         list($existing_pseudo, $existing_password) = explode('|', $user);
         if ($existing_pseudo === $pseudo) {
-            
-            return "Erreur : Ce pseudo est déjà utilisée.";
+            return "Erreur : Ce pseudo est déjà utilisé.";
         }
     }
 
-    // L'adresse pseudo n'existe pas, ajouter le nouvel utilisateur
+    // Le pseudo n'existe pas, ajouter le nouvel utilisateur
     $new_user = "$pseudo|$password" . PHP_EOL;
     if (file_put_contents($file_users, $new_user, FILE_APPEND | LOCK_EX) === false) {
         return "Erreur lors de l'inscription de l'utilisateur.";
@@ -36,6 +34,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 } else {
     echo "Erreur : Le formulaire n'a pas été soumis.";
 }
-
 ?>
 
