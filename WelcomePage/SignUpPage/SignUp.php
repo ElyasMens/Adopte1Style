e0e11a662862673877c89214a1d84b0674fb7804
Rayane93380 <133586286+Rayane93380@users.noreply.github.com>
2024-05-03 22:00:13 +0200

    Update SignUp.php

--- WelcomePage/SignUpPage/SignUp.php
+++ WelcomePage/SignUpPage/SignUp.php
@@ -2,17 +2,17 @@
 function signUp($pseudo, $password) {
     $file_users = 'users.txt';
 
-    // Vérifier si le pseudo existe déjà dans le fichier des utilisateurs
+    // Vérifie si le pseudo existe déjà dans le fichier des utilisateurs
     $users = file($file_users, FILE_IGNORE_NEW_LINES);
     foreach ($users as $user) {
-        list($existing_pseudo, $existing_password) = explode('|', $user);
+        list($existing_pseudo, $existing_password, $existing_category) = explode('|', $user);
         if ($existing_pseudo === $pseudo) {
             return "Erreur : Ce pseudo est déjà utilisé.";
         }
     }
 
-    // Le pseudo n'existe pas, ajouter le nouvel utilisateur
-    $new_user = "$pseudo|$password" . PHP_EOL;
+    // Le pseudo n'existe pas, ajoute le nouvel utilisateur 
+    $new_user = "$pseudo|$password|user" . PHP_EOL;
     if (file_put_contents($file_users, $new_user, FILE_APPEND | LOCK_EX) === false) {
         return "Erreur lors de l'inscription de l'utilisateur.";
     }
@@ -20,7 +20,7 @@ function signUp($pseudo, $password) {
     return "Inscription réussie !";
 }
 
-// Vérifier si le formulaire a été soumis
+// Vérifie si le formulaire a été soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupérer les valeurs saisies par l'utilisateur depuis le formulaire
     $pseudo = $_POST['pseudo'];
