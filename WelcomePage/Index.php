1b592d1b8dd325aecdd0e4c418af10a30c67d302
ElyasMens <133586599+ElyasMens@users.noreply.github.com>
2024-05-04 03:36:45 +0200

    Update Index.php

--- WelcomePage/Index.php
+++ WelcomePage/Index.php
@@ -7,51 +7,49 @@
         <script src="../Assets/JavaScript/Index.js"></script>
     </head>
     <body>
-
-     <!--Header-->
-     <div class="Header">
-        <button onclick="Reveal()" id="InscriptionConnexion">
-            CONNEXION
-        </button>
-    </div>     
-    <div class="Container">
-        <div class="content-c1-l1">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dignissim dapibus orci quis ornare. Fusce neque elit, malesuada non nisl et, molestie pretium nunc. Donec nec imperdiet diam. Aenean ultrices dui elit, a imperdiet justo iaculis eu. Fusce laoreet congue enim, ut tincidunt massa scelerisque ac. Donec magna sem, rhoncus et felis id, consequat volutpat eros. Curabitur a tempor ligula. Integer malesuada, velit sit amet vehicula tempor, diam justo egestas eros, ac lacinia quam odio eu diam. Cras sodales enim felis, vitae gravida lacus porta eget. Etiam consectetur eget neque at auctor. </p>
-        </div>
-        <div class="Hidden" id="ConnexionDiv">
-            <form action="" method="post" id="Connexion">
-                <div class="TitleForm"><p>Connexion</p></div>
-                <div class="ConnexionForm">
-                    <label for="identifier">NOM D'UTILISATEUR</label>
-                    <input name="identifier" placeholder="Nom d'utilisateur">
-                    <label for="password">MOT DE PASSE</label>
-                    <input name="password" placeholder="Mot de passe" autocomplete="new-password" minlength="6">
-                    <div class="Option">
-                        <input type="checkbox" name="Memorise" id="Memorise"><label for="Memorise" id="Memorise">Mémoriser</label>
-                        <a href="#">Mot de passe oublié</a>
+        <!--Header-->
+        <div class="Header">
+            <button onclick="Reveal()" id="InscriptionConnexion">
+                CONNEXION
+            </button>
+        </div>     
+        <div class="Container">
+            <div class="content-c1-l1">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dignissim dapibus orci quis ornare. Fusce neque elit, malesuada non nisl et, molestie pretium nunc. Donec nec imperdiet diam. Aenean ultrices dui elit, a imperdiet justo iaculis eu. Fusce laoreet congue enim, ut tincidunt massa scelerisque ac. Donec magna sem, rhoncus et felis id, consequat volutpat eros. Curabitur a tempor ligula. Integer malesuada, velit sit amet vehicula tempor, diam justo egestas eros, ac lacinia quam odio eu diam. Cras sodales enim felis, vitae gravida lacus porta eget. Etiam consectetur eget neque at auctor. </p>
+            </div>
+            <div class="Hidden" id="ConnexionDiv">
+                <form action="" method="post" id="Connexion">
+                    <div class="TitleForm"><p>CONNEXION</p></div>
+                    <div class="ConnexionForm">
+                        <label for="identifier">NOM D'UTILISATEUR</label>
+                        <input name="identifier" placeholder="Nom d'utilisateur">
+                        <label for="password">MOT DE PASSE</label>
+                        <input name="password" placeholder="Mot de passe" autocomplete="new-password" minlength="6">
+                        <div class="Option">
+                            <input type="checkbox" name="Memorise" id="Memorise"><label for="Memorise" id="Memorise">Mémoriser</label>
+                            <a href="#">Mot de passe oublié</a>
+                        </div>
+                        <div><input type="submit" value="Connexion" id="postButton"></div>
                     </div>
-                    <div><input type="submit" value="Connexion" id="postButton"></div>
+            </div>
+                <div class="content-c1-l2">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dignissim dapibus orci quis ornare. Fusce neque elit, malesuada non nisl et, molestie pretium nunc. Donec nec imperdiet diam. Aenean ultrices dui elit, a imperdiet justo iaculis eu. Fusce laoreet congue enim, ut tincidunt massa scelerisque ac. Donec magna sem, rhoncus et felis id, consequat volutpat eros. Curabitur a tempor ligula. Integer malesuada, velit sit amet vehicula tempor, diam justo egestas eros, ac lacinia quam odio eu diam. Cras sodales enim felis, vitae gravida lacus porta eget. Etiam consectetur eget neque at auctor. </p>
                 </div>
+                <div class="content-c2-l2">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dignissim dapibus orci quis ornare. Fusce neque elit, malesuada non nisl et, molestie pretium nunc. Donec nec imperdiet diam. Aenean ultrices dui elit, a imperdiet justo iaculis eu. Fusce laoreet congue enim, ut tincidunt massa scelerisque ac. Donec magna sem, rhoncus et felis id, consequat volutpat eros. Curabitur a tempor ligula. Integer malesuada, velit sit amet vehicula tempor, diam justo egestas eros, ac lacinia quam odio eu diam. Cras sodales enim felis, vitae gravida lacus porta eget. Etiam consectetur eget neque at auctor. </p>
+                </div>
+        
         </div>
-        <div class="content-c1-l2">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dignissim dapibus orci quis ornare. Fusce neque elit, malesuada non nisl et, molestie pretium nunc. Donec nec imperdiet diam. Aenean ultrices dui elit, a imperdiet justo iaculis eu. Fusce laoreet congue enim, ut tincidunt massa scelerisque ac. Donec magna sem, rhoncus et felis id, consequat volutpat eros. Curabitur a tempor ligula. Integer malesuada, velit sit amet vehicula tempor, diam justo egestas eros, ac lacinia quam odio eu diam. Cras sodales enim felis, vitae gravida lacus porta eget. Etiam consectetur eget neque at auctor. </p>
-        </div>
-        <div class="content-c2-l2">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dignissim dapibus orci quis ornare. Fusce neque elit, malesuada non nisl et, molestie pretium nunc. Donec nec imperdiet diam. Aenean ultrices dui elit, a imperdiet justo iaculis eu. Fusce laoreet congue enim, ut tincidunt massa scelerisque ac. Donec magna sem, rhoncus et felis id, consequat volutpat eros. Curabitur a tempor ligula. Integer malesuada, velit sit amet vehicula tempor, diam justo egestas eros, ac lacinia quam odio eu diam. Cras sodales enim felis, vitae gravida lacus porta eget. Etiam consectetur eget neque at auctor. </p>
-        </div>
-    </div>
-
-
-    <!--Footer-->
-    <div class="Footer">
-        <div class="col1-lg1">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div><div class="col2-lg1">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-        <div class="col3-lg1">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-    </div>
+        <!--Footer-->
+        <footer>
+                <div class="col1-lg1">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div><div class="col2-lg1">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div class="col3-lg1">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+        </footer>
     </body>
 </html>
