e59411ceb98e81abf1a09a6c7692d924ca9a909f
ElyasMens <133586599+ElyasMens@users.noreply.github.com>
2024-05-17 00:46:32 +0200

    Delete Authentification directory

--- Authentification/Disconnect/Disconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_destroy();
-$username = $_COOKIE["user"];
-setcookie('user', $username, time()-60*60, '/');
-header("Location: ../../WelcomePage/Index.php");
-exit();
-?>
