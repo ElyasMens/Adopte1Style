18c872739ae0dac1f9563377a9116fdc7378b323
Rayane93380 <133586286+Rayane93380@users.noreply.github.com>
2024-05-17 11:09:46 +0200

    Add files via upload

--- Profile/CreateProfile.php
+++ Profile/CreateProfile.php
@@ -1,8 +1,5 @@
-<!DOCTYPE html>
-<html lang="fr">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Créer un profil</title>
     <link rel="stylesheet" href="../Assets/CSS/CreateProfile.css">
     <script>
