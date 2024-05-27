df7248c15428df1708c46067eb39cf7c48db375e
Rayane93380 <133586286+Rayane93380@users.noreply.github.com>
2024-05-27 13:59:31 +0200

    Update Recherche.php

--- Search/Recherche.php
+++ Search/Recherche.php
@@ -9,14 +9,14 @@
     <section class="search-section">
         <div class="main-container">
                 <div class="search-bar">
-                    <input id="recherche" type="text" name="recherche" placeholder="Recherche un utilisateur..." maxlength="20">
-                    <button id="search-button"type="submit">Rechercher</button>
+                    <input id="search" type="text" name="search" placeholder="Recherche un utilisateur..." maxlength="20">
+                    <button type="button" id="search-button" onclick="searchUsers()">Rechercher</button>
                 </div>
                 <div class="different-choice">
                     <label for="age_min">Âge min:</label>
-                    <input type="number" id="age_min" name="age_min">
+                    <input type="number" id="age_min" name="age_min"min="18">
                     <label for="age_max">Âge max:</label>
-                    <input type="number" id="age_max" name="age_max">
+                    <input type="number" id="age_max" name="age_max"min ="18" max="100">
                     <label for="localisation">Localisation:</label>
                     <input type="text" id="localisation" name="localisation">
                     <label for="gender">Sexe:</label>
@@ -34,26 +34,7 @@
                     <button type="button" id="filter-button" onclick="filterUsers()">Filtrer</button>
                     </div>
             <div class="search-results">
-            <?php //Show_LastUsers(); ?> <!-- FunctionsSQL.php l.209 -->
-                <ul>
-            <li class='user-info show-user' data-username='Elyas' data-birthdate='2004-06-18' data-location='France' data-gender='homme' data-job='Chomeur' data-size='180'>
-                <img src='{$user_pp}' alt='Profile Picture'>
-                <div class='user-info-content'></div>
-                <div class='user-info-option'>
-                    <a href='../Profile/Profil.php' onclick='Set_a_Cookie()'><span class='material-symbols-outlined'>Account_Circle</span></a>
-                    <a href='../Messaging/Messages.php' onclick='Set_a_Cookie()'><span class='material-symbols-outlined'>Chat_Bubble</span></a>    
-                </div> 
-            </li>
-            <li class='user-info show-user' data-username='DiMaria' data-birthdate='2003-04-20' data-location='France' data-gender='femme' data-job='Cuisine' data-size='160'>
-                <img src='{$user_pp}' alt='Profile Picture'>
-                <div class='user-info-content'></div>
-                <div class='user-info-option'>
-                    <a href='../Profile/Profil.php' onclick='Set_a_Cookie()'><span class='material-symbols-outlined'>Account_Circle</span></a>
-                    <a href='../Messaging/Messages.php' onclick='Set_a_Cookie()'><span class='material-symbols-outlined'>Chat_Bubble</span></a>    
-                </div> 
-            </li>
-            </div>
-</ul>
+            <?php Show_LastUsers(); ?> <!-- FunctionsSQL.php l.209 -->
             </div>
     
         </div>
