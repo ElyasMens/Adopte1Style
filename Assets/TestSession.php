7fc8fc4b016d18ca4c68d09ffedcd237623b2bf6
ElyasMens <133586599+ElyasMens@users.noreply.github.com>
2024-06-05 19:16:44 +0200

    Delete Assets directory

--- Assets/TestSession.php
+++
@@ -1,7 +0,0 @@
-<?php 
-     session_start();
-     $_SESSION['connected']='user';
-     setcookie('user', 'user_name', time()+60*60, '/');
-     header('Location: ../Home/Accueil.php');
-     exit();
-?>
